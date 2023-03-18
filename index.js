93753442216b2113b4332cca9c3c6aafc264fddf
Phuong-Cat Ngo <phuongcatngo@gmail.com>
2023-03-18 11:00:52 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record){
+    if (record.find(isWin)){
+        return record.find(isWin).year;
+    } return undefined;
+}
+function isWin(game){
+    if (game.result == "W"){
+        return game.year;
+    }
+}
